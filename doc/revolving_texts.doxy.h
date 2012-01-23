14d41f0cef06ea6f37fe3f67d5d903383cb620e7
Catherine Burvelle <catherine@taodyne.com>
2012-01-23 15:08:08 +0100

    Merge branch 'master' of ssh://nano.taodyne.com/~ddd/Work/tao
    Conflicts:
    	modules/vlc_audio_video

--- doc/revolving_texts.doxy.h
+++ doc/revolving_texts.doxy.h
@@ -2,7 +2,7 @@
  * @~english
  * @taomoduledescription{RevolvingTexts, Revolving Texts}
  *
- * Revolving texts.\n
+ * Revolving texts.
  *
  * This module allows to add texts rotating around a virtual cylinder in your presentations.
  *
@@ -18,7 +18,7 @@
  * @~french
  * @taomoduledescription{RevolvingTexts, Barillet de textes}
  *
- * Barillet de textes.\n
+ * Barillet de textes.
  *
  * Ce module permet d'ajouter des textes tournants autour d'un cylindre invisible dans vos présentations.
  *
